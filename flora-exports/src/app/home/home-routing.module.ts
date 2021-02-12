330684b46770a8233a4dc1cb95f9b4b7c592d178
Md Nasirul Haque <talk2lurisan@gmail.com>
2021-02-13 01:16:55 +0530

    header and menu items completed

--- flora-exports/src/app/home/home-routing.module.ts
+++ flora-exports/src/app/home/home-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { UnderConstructionComponent } from './under-construction/under-construction.component';
 
 const routes: Routes = [{
   path: '',
   pathMatch: 'full',
-  redirectTo: 'under-construction'
+  redirectTo: 'home'
 },
 {
-  path: 'under-construction',
-  component: UnderConstructionComponent
+  path: 'home',
+  component: HomeComponent
 }];
 
 @NgModule({
